56c16a2af2fd1418b4c534928c899cfe68e57106
john <murdoze357@gmail.com>
2024-02-20 22:08:40 +0200

    Added instruction about configuring plugin paths for development

--- lua/caprecorn/vim.lua
+++ lua/caprecorn/vim.lua
@@ -32,4 +32,19 @@ M.setup = function()
   })
 end
 
+-- How to configure plugin for development
+-- See ~/.config/nvim/init.lua
+--[[
+-- Development
+local plugin_path = "/home/john/src/caprecorn"
+vim.opt.rtp:prepend(plugin_path)
+package.cpath = plugin_path .. "/lib/?.so;" .. package.cpath
+package.path = plugin_path .. "/lua/caprecorn/?.lua;" .. package.path
+require("caprecorn")
+local loaded, caprecorn = pcall(require, 'caprecorn')
+if loaded then
+  _G["caprecorn"] = caprecorn
+end
+--]]
+
 return M
