6eb713d3512667c454004ee83a59de71416df553
Roman Pavlyuk <murdoze357@gmail.com>
2024-03-12 00:44:00 +0200

    Debug log file is re-open for append on each line logged

--- lua/caprecorn/_log.lua
+++ lua/caprecorn/_log.lua
@@ -4,9 +4,12 @@ local M = {}
 local filename = "./caprecorn.log"
 
 local file = io.open(filename, "w+")
+file:close()
 
 M.write = function(line)
+  file = io.open(filename, "a+")
   file:write(line, "\n")
+  file:close()
 end
 
 return M
