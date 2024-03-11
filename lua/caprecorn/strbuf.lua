49a09628c1893b9479cc29dd9fcaddf98c9544b1
Roman Pavlyuk <murdoze357@gmail.com>
2024-03-11 02:36:10 +0200

    Big- and little-endian support. Convenience functions i8()..i64() for little-endian

--- lua/caprecorn/strbuf.lua
+++ lua/caprecorn/strbuf.lua
@@ -40,3 +40,58 @@ string.append = function(dst, src, bytes)
   return dst .. string.from(src, bytes)
 end
 
+-- Extracts big-endian value from a byte string
+-- Index is 0-based
+string.bytes_be = function(s, pos, count)
+  local res = 0
+  local till = pos + 1 + count
+  local size = #s
+  if till > size then
+    till = size
+  end
+  for i = pos + 1, till do
+    local c = s:sub(i, i)
+    local b = string.byte(c)
+    res = res * 256 + b
+  end
+
+  return res
+end
+
+string.bytes_le = function(s, pos, count)
+  if pos == nil then
+    pos = 0
+  end
+  local res = 0
+  local till = pos + count
+  local size = #s
+  if till > size then
+    till = size
+  end
+  local scale = 1
+  for i = pos + 1, till do
+    local c = s:sub(i, i)
+    local b = string.byte(c)
+    res = res + b * scale
+    scale = scale * 256
+  end
+
+  return res
+end
+
+string.i8 = function(s, ofs)
+  return s:bytes_le(ofs, 1)
+end
+
+string.i16 = function(s, ofs)
+  return s:bytes_le(ofs, 2)
+end
+
+string.i32 = function(s, ofs)
+  return s:bytes_le(ofs, 4)
+end
+
+string.i64 = function(s, ofs)
+  return s:bytes_le(ofs, 8)
+end
+
